a947998ae086012a5ab7d68ffe5b5e771f5714b9
Sreesanth Vazhayil <sreesanthv@gmail.com>
2021-08-07 11:39:51 +0530

    Telgram api rate limit

--- src/notification/telegram.go
+++ src/notification/telegram.go
@@ -7,29 +7,49 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
+	"time"
 )
 
 type TelegramNotifier struct {
-	apiUrl string
+	apiUrl  string
+	content []string
+	lock    sync.Mutex
 }
 
 func NewTelegramNotifier(token, chatId string) *TelegramNotifier {
-	return &TelegramNotifier{
+	n := &TelegramNotifier{
 		apiUrl: fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=", token, chatId),
+	}
+	go n.Send()
+
+	return n
+}
+
+func (n *TelegramNotifier) Send() {
+	for {
+		time.Sleep(30 * time.Millisecond)
+		if len(n.content) == 0 {
+			continue
+		}
+
+		res, err := http.Get(n.apiUrl + url.QueryEscape(strings.Join(n.content, "\n\n")))
+		if err != nil {
+			log.Println("Error sending alert to telegram channel:", err)
+		} else if res.StatusCode != 200 {
+			buf := new(strings.Builder)
+			io.Copy(buf, res.Body)
+			log.Println("Error sending alert to telegram channel:", buf.String())
+		}
+		n.content = []string{}
 	}
 }
 
 func (n *TelegramNotifier) Notify(content []string) error {
 	text := strings.Join(content, "\n")
 	text = strings.ReplaceAll(text, "*", "")
-	res, err := http.Get(n.apiUrl + url.QueryEscape(text))
-	if err != nil {
-		log.Println("Error sending alert to telegram channel:", err)
-	} else if res.StatusCode != 200 {
-		buf := new(strings.Builder)
-		io.Copy(buf, res.Body)
-		log.Println("Error sending alert to telegram channel:", buf.String())
-	}
-
+	n.lock.Lock()
+	n.content = append(n.content, text)
+	n.lock.Unlock()
 	return nil
 }
